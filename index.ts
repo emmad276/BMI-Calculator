d53c6c698c6984b3ae76cf87c734e66195a67ded
emmad276 <emmadahmed90@gmail.com>
2024-06-20 10:34:14 -0700

    BMI Calculator
    ADD file

---
+++ index.ts
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+let input1 = await inquirer.prompt([
+    {name : "weight",
+    type: "number",
+    message : "Enter your weight in kgs"
+    }
+]);
+let input2 = await inquirer.prompt([
+    {name: "height",
+    type: "number",
+    message: "Enter you height in meters"
+    }
+]);
+let bmi = input1.weight / (input1.weight * input2.height)
+console.log(`Your BMI is ${bmi}`);
